ad1d7f8d76b5b091e108ada7e7b5735b48fb2f18
Fillipe Duarte <lipeduoli@gmail.com>
2018-07-11 10:40:25 -0300

    add detail

--- app/src/main/java/com/example/navigationplayground/main/AgendaFragment.kt
+++ app/src/main/java/com/example/navigationplayground/main/AgendaFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.navigationplayground.R
+import kotlinx.android.synthetic.main.fragment_agenda.*
 
 
 class AgendaFragment : Fragment() {
@@ -15,6 +17,15 @@ class AgendaFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_agenda, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonDetail.setOnClickListener {
+            view.findNavController().navigate(AgendaFragmentDirections.openDetail())
+        }
+
     }
 
 
