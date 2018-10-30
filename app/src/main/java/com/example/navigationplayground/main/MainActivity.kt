320171dcd6c03da4f445fd0edb1100ff2605845f
Fillipe Duarte <lipeduoli@gmail.com>
2018-10-30 12:05:16 -0300

    popup behavior test

--- app/src/main/java/com/example/navigationplayground/main/MainActivity.kt
+++ app/src/main/java/com/example/navigationplayground/main/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.example.navigationplayground.main
 
-
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import com.example.navigationplayground.R
 import kotlinx.android.synthetic.main.activity_main.*
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,8 +14,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val navController = findNavController(R.id.mainNavigationFragment)
-        setupActionBarWithNavController(this, navController)
+
         NavigationUI.setupWithNavController(bottomNavigationView, navController)
+        NavigationUI.setupWithNavController(toolbar, navController)
 
     }
 
